35d3799a5932bfa42b8cdbee1b7e612fd83b940a
Pijar Karisma <pijarkarisma@gmail.com>
2022-10-26 11:45:13 +0700

    create login page ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,39 +17,40 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label class="text-xl" for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-3 w-full h-[50px]" type="email" name="email" placeholder="nama@alamatemail.com" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <div class="mt-10">
+                <x-label class="text-xl" for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-3 w-full h-[50px]" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            <div class="block mt-5">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-md text-gray-600">{{ __('Ingat saya') }}</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+            <div class="mt-20 grid grid-rows-2">
+                <div>
+                    <x-button>
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+                @if (Route::has('password.request'))
+                <div class="flex justify-center">
+                    <a class="underline text-md text-center font-medium text-dss-purple hover:text-gray-900 mt-5" href="{{ route('password.request') }}">
+                        {{ __('Lupa password?') }}
+                    </a>
+                </div>
+                @endif
             </div>
         </form>
     </x-auth-card>
--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,18 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+<div class="min-h-screen grid xl:grid-cols-3 md:grid-cols-2">
+    <div class="bg-white py-[5rem] px-16">
+        <p class="text-5xl font-bold pb-5">Log in</p>
+        <p class="text-xl font-normal text-gray-400 lg:w-3/4">Silahkan masuk menggunakan akun yang sudah terdaftar</p>
+        <!-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"> -->
+        <div class="mt-10">
+            {{ $slot }}
+        </div>
     </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="bg-[#FAF4FF] xl:col-span-2 py-[5rem] px-16">
+        <p class="w-full flex justify-center lg:text-3xl md:text-xl font-normal text-center">Sistem Pengambilan
+            Keputusan<br>Seleksi Penerimaan Beasiswa<br>Unika Atma Jaya</p>
+        <p class="lg:text-5xl font-semibold text-center mt-20 text-dss-purple">Selamat Datang</p>
+        <div class="flex justify-center mt-20">
+            <img src="/images/logo_atmajaya.png">
+        </div>
     </div>
 </div>
