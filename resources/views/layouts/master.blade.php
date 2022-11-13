c6a482f71dcd8a7827c0453f3722c715611b4382
Pijar Karisma <pijar.karisma@commsult.id>
2022-11-13 20:21:44 +0700

    create sidebar menu

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SPK Beasiswa Atma Jaya</title>
+
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    @livewireStyles
+</head>
+<body class="bg-dss-light-purple">
+    @yield('content')
+    
+    @livewireScripts
+</body>
+</html>
